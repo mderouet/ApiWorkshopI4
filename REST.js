3f0346c9d59e733b55d666bb2728d01a0d4562a7
Martin Derouet <martin.derouet0@gmail.com>
2017-04-11 16:44:13 +0200

    Implementation tenaille

--- REST.js
+++ REST.js
@@ -1,99 +1,281 @@
-
-function REST_ROUTER(router,connection,md5) {
-    var self = this;
-    self.handleRoutes(router,connection,md5);
+function REST_ROUTER(router, connection, md5) {
+  var self = this;
+  self.handleRoutes(router, connection, md5);
 }
 
-REST_ROUTER.prototype.handleRoutes = function(router,md5) {
-    var self = this;
-    // board
-    var board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+REST_ROUTER.prototype.handleRoutes = function(router, md5) {
+  var self = this;
+  // board
+  var board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]
 
-    //Gamers parameters
-    var joueur1 = {idJoueur:null,nomJoueur:null,tenaille:null}
-    var joueur2 = {idJoueur:null,nomJoueur:null,tenaille:null}
-    var partie = {lap:getRandomInt(1,2),status:null, lastCoup:{x:null,y:null},endOfGame:false, prolongation:false }
-    var time=0;
+  //Gamers parameters
+  var joueur1 = {
+    idJoueur: null,
+    nomJoueur: null,
+    tenaille: null
+  }
+  var joueur2 = {
+    idJoueur: null,
+    nomJoueur: null,
+    tenaille: null
+  }
+  var partie = {
+    lap: getRandomInt(1, 2),
+    status: null,
+    lastCoup: {
+      x: null,
+      y: null
+    },
+    endOfGame: false,
+    prolongation: false
+  }
+  var time = 0;
 
-    router.get("/",function(req,res){
-        res.json({"Message" : "Hello World !"});
+  router.get("/", function(req, res) {
+    res.json({
+      "Message": "Hello World !"
     });
+  });
 
-    router.get("/connect/:groupName",function(req,res){
-                if(joueur1.idJoueur == null)
-                {
-                  joueur1.nomJoueur = req.params.groupName
-                  joueur1.idJoueur = md5(req.params.groupName)
-                  res.status(200).send({idjoueur:md5(req.params.groupName),code:200,nomJoueur:req.params.groupName});
-                }
-                else if(joueur2.idJoueur == null)
-                {
-                  joueur2.nomJoueur = req.params.groupName
-                  joueur2.idJoueur = md5(req.params.groupName)
-                  res.status(200).send({idjoueur:md5(req.params.groupName),code:200,nomJoueur:req.params.groupName});
-                }
-                else
-                {
-                  res.status(401).send({code:401});
-                }
-    });
-
-    router.get("/play/:x/:y/:idJoueur",function(req,res){
-      //Id du joueur ne correspond ni au md5 du joueur1, ni du joueur 2
-      if(req.params.idJoueur != joueur1.idJoueur && req.params.idJoueur != joueur2.idJoueur)
-      {
-      res.status(401).send({code:401});
-      }
-      else {
-          //Les coordonnées sont acceptables
-          if (isPositionInBound(req.params.x, req.params.y)) {
-              //Le joueur 1 place un pion
-              if (req.params.idJoueur == joueur1.idJoueur) {
-                  board[req.params.x][req.params.y] = 1
-                  res.status(200).send({code: 200});
-              }
-              //Le joueur 2 place un pion
-              if (req.params.idJoueur == joueur2.idJoueur) {
-                  board[req.params.x][req.params.y] = 2
-                  res.status(200).send({code: 200});
-              }
-          }
-          //Les coordonnées ne sont pas acceptables
-          else {
-              res.status(406).send({code: 406});
-          }
-      }
+  router.get("/connect/:groupName", function(req, res) {
+    if (joueur1.idJoueur == null) {
+      joueur1.nomJoueur = req.params.groupName
+      joueur1.idJoueur = md5(req.params.groupName)
+      res.status(200).send({
+        idjoueur: md5(req.params.groupName),
+        code: 200,
+        nomJoueur: req.params.groupName
       });
-        router.get("/turn/:idJoueur", function(req,res){
-            res.status(200).send({status:0,tableau:board,nbTenaillesJ1:joueur1.tenaille,nbTenaillesJ2:joueur2.tenaille,dernierCoupX:partie.lastCoup.x,dernierCoupY:partie.lastCoup.y,prolongation:partie.prolongation,finPartie:partie.endOfGame,detailFinPartie:"",numTour:partie.lap,code:200});
-        });
+    } else if (joueur2.idJoueur == null) {
+      joueur2.nomJoueur = req.params.groupName
+      joueur2.idJoueur = md5(req.params.groupName)
+      res.status(200).send({
+        idjoueur: md5(req.params.groupName),
+        code: 200,
+        nomJoueur: req.params.groupName
+      });
+    } else {
+      res.status(401).send({
+        code: 401
+      });
+    }
+  });
 
-    // Les valeurs [x,y] sont dans la board et la position est disponible
-    function isPositionInBound(coordX,coordY)
-    {
-      return((coordX >= 0 && coordX <= 18) && (coordY >=0 && coordY <= 18) && (board[coordX][coordY] == 0))
+  router.get("/play/:x/:y/:idJoueur", function(req, res) {
+    //Id du joueur ne correspond ni au md5 du joueur1, ni du joueur 2
+    if (req.params.idJoueur != joueur1.idJoueur && req.params.idJoueur != joueur2.idJoueur) {
+      res.status(401).send({
+        code: 401
+      });
+    } else {
+      //Les coordonnées sont acceptables
+      if (isPositionInBound(req.params.y, req.params.x) && isPositionAvailable(req.params.y, req.params.x)) {
+        //Le joueur 1 place un pion
+        if (req.params.idJoueur == joueur1.idJoueur) {
+          board[req.params.y][req.params.x] = 1
+          res.status(200).send({
+            code: 200
+          });
+        }
+        //Le joueur 2 place un pion
+        if (req.params.idJoueur == joueur2.idJoueur) {
+          board[req.params.y][req.params.x] = 2
+          res.status(200).send({
+            code: 200
+          });
+        }
+      }
+      //Les coordonnées ne sont pas acceptables
+      else {
+        res.status(406).send({
+          code: 406
+        });
+      }
     }
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+  });
+  router.get("/turn/:idJoueur", function(req, res) {
+    res.status(200).send({
+      status: 0,
+      tableau: board,
+      nbTenaillesJ1: joueur1.tenaille,
+      nbTenaillesJ2: joueur2.tenaille,
+      dernierCoupX: partie.lastCoup.x,
+      dernierCoupY: partie.lastCoup.y,
+      prolongation: partie.prolongation,
+      finPartie: partie.endOfGame,
+      detailFinPartie: "",
+      numTour: partie.lap,
+      code: 200
+    });
+  });
+
+
+
+  function tenailleNumber(y, x) {
+    var nombreTenaille = 0
+    var value = board[y][x]
+    var inverse = 1
+    if (value == 1) {
+      inverse = 2
     }
+
+    //Diagonale haut gauche
+    //Position possible
+    if (isPositionInBound(y - 3, x - 3)) {
+      //Présence d'un encadrement
+      if (board[y - 3][x - 3] == value) {
+        //Si les positions existent on vérifie la valeur des cases
+        if (board[y - 1][x - 1] == inverse && board[y - 2][x - 2] == inverse) {
+          board[y - 1][x - 1] = 0
+          board[y - 2][x - 2] = 0
+
+          nombreTenaille++
+        }
+      }
+    }
+
+    //Haut
+    //Position possible
+    if (isPositionInBound(y - 3, x)) {
+      //Présence d'un encadrement
+      if (board[y - 3][x] == value) {
+        //Si les positions existent on vérifie la valeur des cases
+        if (board[y - 1][x] == inverse && board[y - 2][x] == inverse) {
+          board[y - 1][x] = 0
+          board[y - 2][x] = 0
+
+          nombreTenaille++
+        }
+      }
+    }
+
+    //Diagonale haut droite
+    //Position possible
+    if (isPositionInBound(y - 3, x + 3)) {
+      //Présence d'un encadrement
+      if (board[y - 3][x + 3] == value) {
+        //Si les positions existent on vérifie la valeur des cases
+        if (board[y - 1][x + 1] == inverse && board[y - 2][x + 2] == inverse) {
+          board[y - 1][x + 1] = 0
+          board[y - 2][x + 2] = 0
+
+          nombreTenaille++
+        }
+      }
+    }
+
+    //Droite
+    //Position possible
+    if (isPositionInBound(y, x + 3)) {
+      //Présence d'un encadrement
+      if (board[y][x + 3] == value) {
+        //Si les positions existent on vérifie la valeur des cases
+        if (board[y][x + 1] == inverse && board[y][x + 2] == inverse) {
+          board[y][x - 1] = 0
+          board[y][x - 2] = 0
+
+          nombreTenaille++
+        }
+      }
+    }
+
+    //Diagonale bas droite
+    //Position possible
+    if (isPositionInBound(y + 3, x + 3)) {
+      //Présence d'un encadrement
+      if (board[y + 3][x + 3] == value) {
+        //Si les positions existent on vérifie la valeur des cases
+        if (board[y + 1][x + 1] == inverse && board[y + 2][x + 2] == inverse) {
+          board[y + 1][x + 1] = 0
+          board[y + 2][x + 2] = 0
+
+          nombreTenaille++
+        }
+      }
+    }
+
+    //Bas
+    //Position possible
+    if (isPositionInBound(y + 3, x)) {
+      //Présence d'un encadrement
+      if (board[y + 3][x] == value) {
+        //Si les positions existent on vérifie la valeur des cases
+        if (board[y + 1][x] == inverse && board[y + 2][x] == inverse) {
+          board[y + 1][x] = 0
+          board[y + 2][x] = 0
+
+          nombreTenaille++
+        }
+      }
+    }
+
+
+    //Diagonale bas gauche
+    //Position possible
+    if (isPositionInBound(y + 3, x - 3)) {
+      //Présence d'un encadrement
+      if (board[y + 3][x - 3] == value) {
+        //Si les positions existent on vérifie la valeur des cases
+        if (board[y + 1][x - 1] == inverse && board[y + 2][x - 2] == inverse) {
+          board[y + 1][x - 1] = 0
+          board[y + 2][x - 2] = 0
+
+          nombreTenaille++
+        }
+      }
+    }
+
+
+    //Diagonale gauche
+    //Position possible
+    if (isPositionInBound(y, x - 3)) {
+      //Présence d'un encadrement
+      if (board[y][x - 3] == value) {
+        //Si les positions existent on vérifie la valeur des cases
+        if (board[y][x - 1] == inverse && board[y][x - 2] == inverse) {
+          board[y][x - 1] = 0
+          board[y][x - 2] = 0
+
+          nombreTenaille++
+        }
+      }
+    }
+
+    return nombreTenaille
+  }
+  // Les valeurs [x,y] sont dans la board
+  function isPositionInBound(coordX, coordY) {
+    return ((coordX >= 0 && coordX <= 18) && (coordY >= 0 && coordY <= 18))
+  }
+
+  //la position est disponible (symbolisé par un 0)
+  function isPositionAvailable(coordX, coordY) {
+    return (board[coordX][coordY] == 0)
+  }
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
 
 module.exports = REST_ROUTER;
